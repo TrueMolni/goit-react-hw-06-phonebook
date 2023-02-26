03dc118dde5296823a3cf58df123284fed4e0106
TrueMolni <truemolni@gmail.com>
2023-02-26 19:24:32 +0200

    0.0.3

--- src/components/ContactsItem/ContactsItem.jsx
+++ src/components/ContactsItem/ContactsItem.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
-// import scss from './contacts-item.module.scss';
+import scss from './contacts-item.module.scss';
 
 const ContactsItem = ({ id, name, number, removeContact }) => {
   return (
     <li /*className={scss.item}*/>
       {name}. Number : {number}
       <button
-        /*className={scss.button}*/
+        className={scss.button}
         onClick={() => removeContact(id)}
         type="button"
       >
